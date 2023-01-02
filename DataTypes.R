d247a195acb11797179e1423198ac51d45f1c8f9
pratikpandey13 <pratik.4607@gmail.com>
2023-01-03 03:00:16 +0530

    adding my first repo of R

---
+++ DataTypes.R
@@ -0,0 +1,24 @@
+#Integer 
+# Always add L for integer datatype R will consider by default as Double
+p<- 2L
+typeof((p))
+
+#Double 
+q <- 1.3
+typeof((q))
+
+
+#complex 
+r= 3+ 2i
+typeof(r)
+
+#character 
+y= "Yami"
+typeof(y)
+
+
+#logical 
+t= TRUE 
+typeof(t)
+f= FALSE
+typeof(f)
